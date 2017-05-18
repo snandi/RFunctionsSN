0d36c09bd0f05284b80e3ba6f1febe49c40e1a38
snandi <nandi@stat.wisc.edu>
2017-05-17 22:48:08 -0500

    Added documentation of colSD

--- R/colSD.R
+++ R/colSD.R
@@ -1,9 +1,29 @@
+#' @title Standard deviation of each column
+#' 
+#' @description Gets the standard deviation (SD) of each column of a matrix or data-frame
+#' 
+#' @param Matrix A \code{matrix} object or a \code{data-frame}
+#' 
+#' @details The values should be all numeric
+#' 
+#' @return Returns a vector of \code{length = ncol(Matrix)} with the column SDs
+#' 
+#' @author Subhrangshu Nandi, PhD Statistics, UW Madison; snandi@wisc.edu or nands31@gmail.com
+#' 
+#' @seealso \code{rowSD}
+#' 
+#' @examples 
+#' data( LifeCycleSavings )
+#' colSD( Matrix = t( LifeCycleSavings ) )
+#' 
+#' @keywords standard-deviation
+#' 
 #' @export
 ################################################################## 
 ## Returns the SD of each column of a dataset
 ##################################################################
 colSD <- function( Matrix ){
-  SD <- apply(X = Matrix, MARGIN = 2, FUN = function(Row){sd(Row)})
-  return(SD)
+  SD <- apply( X = Matrix, MARGIN = 2, FUN = function( Row ){ sd( Row ) } )
+  return( SD )
 }
 ################################################################## 
